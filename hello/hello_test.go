d3cd23f1f4783e436e89b827681655929782a78a
yuta0805 <yuta-hamada@freee.co.jp>
2024-01-24 14:30:26 +0900

    error messageを共通化

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,22 +3,24 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
+    assertCorrectMessage := func(t testing.TB, got, want string) {
+        t.Helper() // テスト失敗時に行番号を報告するために必要
+        if got != want {
+            t.Errorf("got %q want %q", got, want)
+        }
+    }
     // test内容をグループ化する
     t.Run("saying hello to people", func(t *testing.T) {
         got := Hello("Chris")
         want := "Hello, Chris"
 
-        if got != want {
-            t.Errorf("got %q want %q", got, want)
-        }
+        assertCorrectMessage(t, got, want)
     })
 
     t.Run("say 'Hello world'", func(t *testing.T) {
         got := Hello("")
         want := "Hello, world"
 
-        if got != want {
-            t.Errorf("got %q want %q", got, want)
-        }
+        assertCorrectMessage(t, got, want)
     })
 }
