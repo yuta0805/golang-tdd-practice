1cec3cfecf3b8daaffd42012c8f056711c56fe44
yuta0805 <yuta-hamada@freee.co.jp>
2024-02-21 14:33:23 +0900

    配列の合計とスライスの操作を追加

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -1,5 +1,34 @@
 package arrays
 
-func Sum(arr [5]int) int {
-	
+
+func Sum(arr []int) int {
+	var result int
+	for _, v := range arr {
+		result += v
+	}
+
+	return result
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+// 末尾の要素を除いたスライスの合計を返す
+func SumAllTails(numbersToSum ...[]int) []int {
+    var sums []int
+    for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))	
+		}
+    }
+
+    return sums
 }
--- arrays/arrays_test.go
+++ arrays/arrays_test.go
@@ -1,12 +1,63 @@
 package arrays
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
+// 全てのテストケースを考え実行することは困難なので、テストケースの取捨選択が重要
+// go test -coverd でカバレッジを確認することができる.
 func TestSum(t *testing.T) {
-	arr := []int{1, 2, 3, 4, 5}
-	sum := Sum(arr)
-	expected := 15
-	if sum != expected {
-		t.Errorf("expected %d but got %d", expected, sum)
-	}
+	t.Run("sum of 5 elements", func(t *testing.T) {
+		arr := []int{1, 2, 3, 4, 5}
+		sum := Sum(arr)
+		expected := 15
+		if sum != expected {
+			t.Errorf("expected %d but got %d", expected, sum)
+		}
+	})
+	
+	t.Run("collection of any size", func(t *testing.T) {
+		arr := []int{1, 2, 3}
+		sum := Sum(arr)
+		expected := 6
+		if sum != expected {
+			t.Errorf("expected %d but got %d", expected, sum)
+		}
+	})
 } 
+
+
+// slice統合演算子を使うことはできない。なのでreflect.DeepEqualを使う
+// reflect.DeepEqualは、2つの変数が同じであるかを確認するために便利
+// reflect.DeepEqualは型安全ではない。なのでコンパイルが通ってしまう。
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1,2}, []int{0,9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got: %v, want: %v", got, want)
+	}
+}
+func TestSumAllTails(t *testing.T) {
+	// アサーションにerrorハンドリングを委譲する
+    checkSums := func(t *testing.T, got, want []int) {
+        t.Helper()
+        if !reflect.DeepEqual(got, want) {
+            t.Errorf("got %v want %v", got, want)
+        }
+    }
+
+    t.Run("make the sums of tails of", func(t *testing.T) {
+        got := SumAllTails([]int{1, 2}, []int{0, 9})
+        want := []int{2, 9}
+        checkSums(t, got, want)
+    })
+
+    t.Run("safely sum empty slices", func(t *testing.T) {
+        got := SumAllTails([]int{}, []int{3, 4, 5})
+        want := []int{0, 9}
+        checkSums(t, got, want)
+    })
+
+}
